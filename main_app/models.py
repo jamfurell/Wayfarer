79dca3c37284a6a0529bdbac40e4788ed69c7119
devinlb <hhodvp@hotmail.com>
2021-03-16 13:19:10 -0600

    Connects User and Profile

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -13,7 +15,13 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, null =True,)
     def __str__ (self):
-        return self.user 
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
 
 class City(models.Model):
     name= models.CharField(max_length=100)
