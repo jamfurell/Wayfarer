e0ea2722228d998b53a73e89cce809f1202fc42c
devinlb <hhodvp@hotmail.com>
2021-03-17 17:08:29 -0600

    Adds basic city show pages and default city image if none is available

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about_us/', views.about_us, name='about_us'),
     path('profile/', views.profile, name='profile'),
-    path('search/', views.search_results, name='search_results'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('search/', views.search_results, name='search_results'),
+    path('cities/<int:city_id>/', views.show_city, name='show_city'),
     path('review/<int:review_id>/', views.show_review, name='show_review'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
