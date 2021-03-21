dc4530d0471f09c6d7dd69e4c63e8a59b160b148
devinlb <hhodvp@hotmail.com>
2021-03-20 22:08:54 -0600

    Fixes settings

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -81,8 +81,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'wayfarer',
-        'USER': 'postgres',
-        'PASSWORD': '3127',
     }
 }
 
